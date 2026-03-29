00178f6e06087da71c1ad69255fecfe9d739a466
rohitchugh7 <rohitchugh.mca@gmail.com>
2026-03-29 22:26:18 +0530

    chore: sync pages/LoginPage.js

---
+++ pages/LoginPage.js
@@ -0,0 +1,25 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.getByRole('textbox', { name: 'Username' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.errorMessage = page.locator('[data-test="error"]');
+  }
+
+  async navigate(url) {
+    await this.page.goto(url, { waitUntil: 'domcontentloaded' });
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async getErrorMessage() {
+    return this.errorMessage.textContent();
+  }
+}
+
+module.exports = { LoginPage };
